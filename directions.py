f44147cf680585657680a1554d6c3331bb40ddf8
suvir <mrsuvir@gmail.com>
2023-09-14 22:41:27 +0100

    hfsdf

--- directions.py
+++ directions.py
@@ -12,9 +12,13 @@ end_coords = (-1.5358874132664084,53.80426084639122)
 directions = client.directions(
     coordinates=[start_coords, end_coords],
     profile='driving-car',
-    format="WKT"
+    format="json"
 )
 print(directions)
 
-
-  
+# Extract and print only the coordinates
+if 'features' in directions:
+    for feature in directions['features']:
+        if 'geometry' in feature:
+            coordinates = feature['geometry']['coordinates']
+            print(coordinates)
