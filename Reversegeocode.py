3d28931566802738f3733b742442b78236855c4c
suvir <mrsuvir@gmail.com>
2023-09-16 14:03:37 +0100

    comment

--- Reversegeocode.py
+++ Reversegeocode.py
@@ -14,10 +14,10 @@ geolocator = Nominatim(user_agent="my_geocoder", scheme="http")
 def reverse_geocode(latitude, longitude):
     # Perform reverse geocoding
     location = geolocator.reverse((latitude, longitude), language="en")
+    # Extract the place name
     place_name = location.address 
     return place_name
-    # Extract the place name
-
+    
     
    
 
