0d527c7ac57864287b1b69dc47958ad845bde989
suvir <mrsuvir@gmail.com>
2023-09-18 22:33:56 +0100

    fix to previous commit

--- accept_passengers.py
+++ accept_passengers.py
@@ -23,16 +23,13 @@ class AcceptPassenger(customtkinter.CTkToplevel):
         self.passenger_end_lon = self.passenger_info.get('passenger_end_coordinates')[1]
         
         self.passenger_label = customtkinter.CTkLabel(master = self.passenger_requests_frame,text= f"Passenger Name: {self.passenger_info['passenger_name']}, " f"Age: {self.passenger_info['passenger_age']}, "f"Pickup Location: {self.passenger_start_lat}, {self.passenger_start_lon}, "f"Final Destination: {self.passenger_end_lat}, {self.passenger_end_lon}")
-
-
-
         self.passenger_label.grid(pady=(20, 0), padx=(20, 20), row=0, column=0,sticky = "ew")
 
         self.accept_button = customtkinter.CTkButton(master=self.passenger_requests_frame, text="Accept", command=self.accept_incoming_passenger)
-        self.accept_button.grid(row=1, column=1,pady=(20, 0), padx=(20, 20),sticky = "ew")
+        self.accept_button.grid(row=2, column=1,pady=(20, 0), padx=(20, 20),sticky = "ew")
 
         self.decline_button = customtkinter.CTkButton(master = self.passenger_requests_frame,text="Decline")
-        self.decline_button.grid(row=1, column=3,pady=(20, 0), padx=(20, 20),sticky = "ew")
+        self.decline_button.grid(row=2, column=2,pady=(20, 0), padx=(20, 20),sticky = "ew")
 
     def accept_incoming_passenger(self):
         self.accepted_passenger_info = {
